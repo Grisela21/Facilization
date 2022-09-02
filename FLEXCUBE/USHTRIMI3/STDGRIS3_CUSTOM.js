872f17b13925b969ff74f069e10152b8904dc11e
FACILIZATION\grisela.daja <grisela.daja@gmail.com>
2022-09-02 12:20:26 +0200

    no message

---
+++ FLEXCUBE/USHTRIMI3/STDGRIS3_CUSTOM.js
@@ -0,0 +1,19 @@
+function fnFunctionCustom()
+{
+	appendData();
+	debugs('FCZ Start', gAction);
+	var prevgAction=gAction;
+	gAction='POPULATE';
+	fcjRequestDOM=buildUBSXml();
+	debugs('FCZ', fcjRequestDOM);
+	fcjResponseDOM=fnPost(fcjRequestDOM, servletURL,functionId);
+	debugs('FCZ req', fcjRequestDOM);
+	debugs('FCZ resp', fcjResponseDOM);
+	if(!fnProcessResponse()){
+	return true;
+	}
+	debugs('FCZ final', fcjResponseDOM);
+	
+	gAction=prevgAction;
+	
+}
